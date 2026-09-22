0ef7ffcee83b7a7ebd3e084758a916736e9cc242
Chris <nocturney@gmail.com>
2026-09-22 18:03:17 +0300

    Prepare STOW 0.4.0-preview.6

--- src/STOWSetup.cs
+++ src/STOWSetup.cs
@@ -16,11 +16,11 @@ using Microsoft.Win32;
 [assembly: AssemblyCopyright("Copyright (c) 2026 Christian Velvet")]
 [assembly: AssemblyVersion("0.4.0.0")]
 [assembly: AssemblyFileVersion("0.4.0.0")]
-[assembly: AssemblyInformationalVersion("0.4.0-preview.5")]
+[assembly: AssemblyInformationalVersion("0.4.0-preview.6")]
 
 internal static class SetupConstants
 {
-    public const string Version = "0.4.0-preview.5";
+    public const string Version = "0.4.0-preview.6";
     public const string FileVersion = "0.4.0.0";
     public const string Publisher = "Christian Velvet";
     public const string ProductName = "STOW";
